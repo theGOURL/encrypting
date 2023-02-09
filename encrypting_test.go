53b9cecd829d28f0a5660fe84e4a990f1a3a4892
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-09 10:32:58 -0300

    Adding new Example Test

--- encrypting_test.go
+++ encrypting_test.go
@@ -7,3 +7,11 @@ func ExampleEncrypt(){
   //Output:
   //Aproved
 }
+
+func ExampleValidatePassword(){
+   password := "haggafhu1837uhud";
+   generatePasswordEncrypted :=  []byte("$2a$10$r4zkLfYMLY5meIBlRLTT2eEeeA0fVwZUTqZ3HibVSgSxFSS2UImGK");
+     ValidatePassword(password,generatePasswordEncrypted);
+  //Output:
+  //Aproved
+}
